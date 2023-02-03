1c8bfdbf2465d8f655b6ecf86b20b7bd79feb934
AJIyanu <aderemijoseph@live.com>
2023-02-03 04:38:46 +0100

    new method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -13,3 +14,23 @@ def filter_datum(fields: List[str], redaction: str,
         rep = field + "=" + redaction + separator
         message = (re.sub(patt, rep, message))
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """initializes the class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """log me in that format"""
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                              record.getMessage(), self.SEPARATOR)
+        return logging.Formatter(self.FORMAT).format(record)
