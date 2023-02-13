241c67a33b7b6ef7b4d33a4e4f0ff07a5f6b994a
AJIyanu <aderemijoseph@live.com>
2023-02-13 21:28:53 +0100

    fixing

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,24 +3,3 @@
 This is authentication module longer long
 """
 
-
-from flask import Request
-from typing import List, TypeVar
-
-
-class Auth:
-    """Authenticate me please
-    how long do I  need"""
-
-
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """requires authenticatiion here, returns False """
-        return False
-
-    def authorization_header(self, request=None) -> str:
-        """does nothing yet, returns none"""
-        return None
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """does nothing too, returns none"""
-        return None
