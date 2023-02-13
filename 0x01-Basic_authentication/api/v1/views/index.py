80954eb17404179c7df1bbb26a5c7a8b97e9672d
AJIyanu <aderemijoseph@live.com>
2023-02-13 12:07:52 +0100

    added forbidden error 403

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> str:
     """test error 401"""
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """test error 403"""
+    abort(403)
