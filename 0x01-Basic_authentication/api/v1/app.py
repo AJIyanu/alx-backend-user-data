f8fa4153a883dde24c5ba72fb65b0904f05bef6d
AJIyanu <aderemijoseph@live.com>
2023-02-15 12:05:12 +0100

    debug

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,6 +22,7 @@ if getenv("AUTH_TYPE", None) is not None:
 @app.before_request
 def handle_before(auth):
     """handles stuff before request is made"""
+    print("we are here")
     nothing = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if auth is None:
         pass
