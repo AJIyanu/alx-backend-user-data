2a2d33e4c237f806234b4c7953e909666a624f25
AJIyanu <aderemijoseph@live.com>
2023-02-15 14:06:38 +0100

    pycoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,8 +8,8 @@ from .auth import Auth
 class BasicAuth(Auth):
     """A basic authentity class"""
 
-
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """extract base64 authorizzation header"""
         if authorization_header is None or type(authorization_header) is not str:
             return None
