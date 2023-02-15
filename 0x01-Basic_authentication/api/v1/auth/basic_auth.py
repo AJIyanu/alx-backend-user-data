8cac6839750b4a5894f1ece97f7d453c3ed90b09
AJIyanu <aderemijoseph@live.com>
2023-02-15 13:42:45 +0100

    strip Basic 64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,13 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """A basic authentity class"""
-    pass
+
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """extract base64 authorizzation header"""
+        if authorization_header is None or authorization_header is not str:
+            return None
+        if "Basic " not in authorization_header:
+            return None
+        else:
+            return authorization_header.replace("Basic ", "")
