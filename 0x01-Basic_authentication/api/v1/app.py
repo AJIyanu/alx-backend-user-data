7af7826e9d007d41f53fb4967a4626fbab6ee045
AJIyanu <aderemijoseph@live.com>
2023-02-15 11:33:21 +0100

    before request?

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,28 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv("AUTH_TYPE"):
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def handle_before(auth):
+    """handles stuff before request is made"""
+    nothing = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth is None:
+        pass
+    if auth.require_auth(request.path, nothing):
+        pass
+    if auth.authorization_header(request):
+        abort(401)
+        return None
+    if auth.current_user(request):
+        abort(403)
+        return None
+
 
 
 @app.errorhandler(404)
