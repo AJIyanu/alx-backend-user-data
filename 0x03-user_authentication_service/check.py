f92abbd071521e4a114fd70649b267815c9d6b47
AJIyanu <aderemijoseph@live.com>
2023-02-19 01:42:34 +0100

    no echo

--- 0x03-user_authentication_service/check.py
+++ 0x03-user_authentication_service/check.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+from db import DB
 from user import User
 
-check = User()
-check.email = "email"
-check.hashed_password = "paswd"
+my_db = DB()
 
-print(check)
-print(check.__dict__)
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
+
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
