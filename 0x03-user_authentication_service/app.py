648352064113c06621248e9f81f525ee7e5c3372
AJIyanu <aderemijoseph@live.com>
2023-02-19 19:57:08 +0100

    post registration

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """crrating a basic flask app"""
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +14,19 @@ app = Flask(__name__)
 def payload() -> str:
     """return payload jsonify"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """registers users"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is not None and password is not None:
+        try:
+            user = AUTH.register_user(email, password)
+            return jsonify({"email": user.email, "message": "user created"})
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
