c72279c75cb70a95c97b65ceda4f65967f209298
AJIyanu <aderemijoseph@live.com>
2023-02-19 17:23:39 +0100

    create user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,30 @@ Hashes a password
 """
 
 
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """You alakobian. I will commemt"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """registers a user to database"""
+        db = self._db
+        try:
+            check = db.find_user_by(email=email)
+            raise ValueError("User <user's email> already exists")
+        except NoResultFound:
+            password = _hash_password(password)
+            return db.add_user(email, password)
+
+
 def _hash_password(password: str) -> bytes:
     """returns a byted hashed password"""
     import bcrypt
