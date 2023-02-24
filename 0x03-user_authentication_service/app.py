8fe0416eb211944d9d98961505263b059fc64753
AJIyanu <aderemijoseph@live.com>
2023-02-24 03:47:56 +0100

    rrset me

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,5 +75,16 @@ def profile() -> str:
     return jsonify({"email": user.email})
 
 
+@app.route("/reset_password", methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """password reset token generator"""
+    email = request.form.get('email')
+    try:
+        token = Auth.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": token})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
