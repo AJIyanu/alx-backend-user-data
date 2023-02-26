20c75b1a8a22d7bf4b49564f8d99ce30606aba31
AJIyanu <aderemijoseph@live.com>
2023-02-26 23:18:34 +0100

    fixed

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,7 +20,7 @@ class SessionAuth(Auth):
             return None
         if type(user_id) is not str:
             return None
-        uid = uuid.uuid4()
+        uid = str(uuid.uuid4())
         self.user_id_by_session_id.update({uid: user_id})
         return uid
 
--- 0x02-Session_authentication/check.py
+++ 0x02-Session_authentication/check.py
@@ -3,13 +3,28 @@
 """
 
 if __name__ == "__main__":
-    from api.v1.auth.auth import Auth
 
-    a = Auth()
-    path = "/api/v1/users"
-    paths_excluded = ["/api/v1/us*"]
-    res = a.require_auth(path, paths_excluded)
-    if res:
-        print("require_auth must return True: {} - {}".format(path, paths_excluded))
-        exit(1)
-    print("OK", end="")
+    try:
+        from api.v1.auth.session_auth import SessionAuth
+        sa = SessionAuth()
+        user_id_1 = "User 1"
+        session_id_1 = sa.create_session(user_id_1)
+        if session_id_1 is None:
+            print("Can't create session ID")
+            exit(1)
+       
+        print(session_id_1)
+        new_user_id_1 = sa.user_id_for_session_id(session_id_1)
+        print(new_user_id_1)
+        if new_user_id_1 is None:
+            print("user_id_for_session_id doesn't return the user ID linked to the session ID")
+            exit(1)
+        
+        if new_user_id_1 != user_id_1:
+            print("user_id_for_session_id doesn't return the user ID linked to the session ID created previously")
+            exit(1)
+        
+        print("OK", end="")
+    except:
+        import sys
+        print("Error: {}".format(sys.exc_info()))
