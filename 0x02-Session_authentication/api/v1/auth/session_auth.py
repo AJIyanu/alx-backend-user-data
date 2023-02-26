2cf59f5712428109e6087f7419b4fdde79e104af
AJIyanu <aderemijoseph@live.com>
2023-02-26 17:12:49 +0100

    sessionauth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""It’s the first step for creating
+a new authentication mechanism"""
+
+
+from auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    validate if everything inherits correctly without any overloading
+    validate the “switch” by using environment variables
+    """
+    pass
