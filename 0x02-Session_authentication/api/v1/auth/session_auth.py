3b33cd967a1b5fba3521a2237b094d8e750b6bca
AJIyanu <aderemijoseph@live.com>
2023-02-26 22:45:05 +0100

    get peace

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,4 +30,4 @@ class SessionAuth(Auth):
             return None
         if type(session_id) is not str:
             return None
-        return self.user_id_by_session_id.get('session_id')
+        return self.user_id_by_session_id.get(session_id)
