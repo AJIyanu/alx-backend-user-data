9283d3e8fbb88d8db7e92e9b6e760a245e24b410
AJIyanu <aderemijoseph@live.com>
2023-02-26 14:46:59 +0100

    debug

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -26,10 +26,10 @@ def view_one_user(user_id: str = None) -> str:
       - 404 if the User ID doesn't exist
     """
     if user_id is None:
-        abort(404)
+        return jsonify(["user_id is None"])
     user = User.get(user_id)
     if user is None:
-        abort(404)
+        return jsonify(["I didnt get a user"])
     if user_id == "me":
         if request.current_user is None:
             return jsonify(["no shit"])
