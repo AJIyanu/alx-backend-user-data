e6169f84eae9cb845373f892878eca23235e8312
AJIyanu <aderemijoseph@live.com>
2023-02-26 22:31:32 +0100

    session id generator

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 a new authentication mechanism"""
 
 
+import uuid
 from .auth import Auth
 
 
@@ -11,4 +12,14 @@ class SessionAuth(Auth):
     validate if everything inherits correctly without any overloading
     validate the “switch” by using environment variables
     """
-    pass
+    user_id_by_session_id = {}
+
+
+    def create_session(self, user_id: str = None) -> str:
+        """that creates a Session ID for a user_id"""
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        uid = uuid.uuid4()
+        self.user_id_by_session_id.update({uid: user_id})
