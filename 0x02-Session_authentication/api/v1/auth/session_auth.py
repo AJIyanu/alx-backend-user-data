5d921569fb2fb8e302cdcc54c1cd8af99c408a32
AJIyanu <aderemijoseph@live.com>
2023-02-26 22:43:12 +0100

    get peace

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -23,3 +23,11 @@ class SessionAuth(Auth):
         uid = uuid.uuid4()
         self.user_id_by_session_id.update({uid: user_id})
         return uid
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User ID based on a Session ID"""
+        if session_id is None:
+            return None
+        if type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get('session_id')
