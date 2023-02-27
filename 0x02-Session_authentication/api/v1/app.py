094da4301d37e79bd9d85abd94691d7c3184a896
AJIyanu <aderemijoseph@live.com>
2023-02-27 04:51:36 +0100

    debug

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,7 +38,7 @@ def handle_before():
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.session_cookie(request) is None:
-        abort(401)
+        abort(403)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
