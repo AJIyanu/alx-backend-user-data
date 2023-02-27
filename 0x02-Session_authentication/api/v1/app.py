057abb926892246c9a370b04e09ae016d14370e0
AJIyanu <aderemijoseph@live.com>
2023-02-27 04:26:14 +0100

    sync

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,7 +38,7 @@ def handle_before():
     if auth.authorization_header(request) is None:
         abort(401)
         return None
-    if auth.session_cookie(request) == None:
+    if auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
