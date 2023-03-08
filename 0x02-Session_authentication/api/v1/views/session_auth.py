cd7c6cdd1c30d8a39cce48fff88d6a22e8bce9d8
AJIyanu <aderemijoseph@live.com>
2023-03-08 16:29:02 +0100

    sync

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,4 +4,29 @@ handles all routes for the Session authentication.
 """
 
 
+from api.v1.views import app_views
+from flask import session, jsonify, request
+from models.user import User
+from os import getenv
 
+@app_views.route("/auth_session/login", method=['POST'], strict_slashes=False)
+def login() -> str:
+    """retrieve users instance based on email"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email is None:
+        return jsonify({ "error": "email missing" }), 400
+    if password is None:
+        return jsonify({ "error": "password missing" }), 400
+    user = User.search({"email": email})
+    if len(user) == 0:
+        return jsonify({ "error": "no user found for this email" }), 404
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({ "error": "wrong password" }), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    session_name = getenv("SESSION_NAME")
+    user_json = user.to_json()
+    session[session_name] = session_id
+    return user_json
