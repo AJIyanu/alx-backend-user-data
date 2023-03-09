4a0cc56803356fae0dc74363e1711ca977c5443a
AJIyanu <aderemijoseph@live.com>
2023-03-09 12:02:21 +0100

    sync

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -6,6 +6,7 @@ create expiry date for session.
 
 from os import getenv
 from .session_auth import SessionAuth
+from datetime import datetime, timedelta
 
 
 class SessionExpAuth(SessionAuth):
@@ -23,3 +24,26 @@ class SessionExpAuth(SessionAuth):
         session = super().create_session(user_id)
         if session is None:
             return None
+        sess_dict = {"user_id": user_id,
+                     "created_at": datetime.now()
+                     }
+        self.user_id_by_session_id = {session: sess_dict}
+        return self.user_id_by_session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """overload method here"""
+        if session_id is None:
+            return None
+        if session_id not in self.user_id_by_session_id:
+            return None
+        if self.session_duration <= 0:
+            sess_dict = self.user_id_by_session_id.get(session_id)
+            return sess_dict.get("user_id")
+        created = self.user_id_by_session_id.get("created_at")
+        if created is None:
+            return None
+        current = created + timedelta(seconds=self.session_duration)
+        if datetime.now() < current:
+            return None
+        sess_dict = self.user_id_by_session_id.get(session_id)
+        return sess_dict.get('user_id')
